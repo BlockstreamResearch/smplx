4d43e272f881f15a3128d164158ffd69a515b258
Artem Chystiakov <artem.ch31@gmail.com>
2026-03-06 15:39:49 +0200

    fix build

--- crates/macros-core/src/lib.rs
+++ crates/macros-core/src/lib.rs
@@ -1,12 +1,6 @@
-#![warn(clippy::all, clippy::pedantic)]
-
 pub mod attr;
 pub mod test;
 
-/// Expands the `include_simf` macro.
-///
-/// # Errors
-/// Returns a `syn::Result` with an error if parsing, compilation, or expansion fails.
 pub fn expand_include_simf(input: &attr::parse::SynFilePath) -> syn::Result<proc_macro2::TokenStream> {
     let simf_content = attr::SimfContent::eval_path_expr(input)?;
     let abi_meta = attr::program::compile_simf(&simf_content)?;
@@ -15,11 +9,6 @@ pub fn expand_include_simf(input: &attr::parse::SynFilePath) -> syn::Result<proc
     Ok(generated)
 }
 
-/// Expands the `test` macro.
-///
-/// # Errors
-/// Returns a `syn::Result` with an error if expansion fails.
 pub fn expand_test(args: proc_macro2::TokenStream, input: syn::ItemFn) -> syn::Result<proc_macro2::TokenStream> {
-    // test::expand(args, input)
-    todo!()
+    test::expand(args, input)
 }
--- crates/simplex/src/lib.rs
+++ crates/simplex/src/lib.rs
@@ -1,3 +1,6 @@
+pub extern crate either;
+pub extern crate serde;
+pub extern crate bincode;
 pub extern crate simplicityhl;
 
 pub extern crate simplex_macros;
