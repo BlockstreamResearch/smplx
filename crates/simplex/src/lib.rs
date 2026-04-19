0e015dd46cc338e06c364fd687b8d9df7b70e9a9
Kyryl R <kyryl.ryabov@gmail.com>
2026-04-19 17:43:33 +0300

    simplex: re-export lwk crates used in wallet abi

--- crates/simplex/src/lib.rs
+++ crates/simplex/src/lib.rs
@@ -2,10 +2,16 @@ pub use either;
 pub use serde;
 pub use simplicityhl;
 
+pub use lwk_common;
+pub use lwk_signer;
+pub use lwk_simplicity;
+pub use lwk_wollet;
+
 pub use smplx_sdk::*;
 
 pub use smplx_test::config::TestConfig;
 pub use smplx_test::context::TestContext;
+pub use smplx_test::wallet_abi;
 
 pub use smplx_macros;
 pub use smplx_macros::{include_simf, test};
