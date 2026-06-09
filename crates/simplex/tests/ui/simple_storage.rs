c2e92b75af796110c668979cce2832fb23b64e42
Illia Kripaka <kripakaillya@gmail.com>
2026-06-09 10:38:05 +0300

    simplex: move ui tests code into separate functions

--- crates/simplex/tests/ui/simple_storage.rs
+++ crates/simplex/tests/ui/simple_storage.rs
@@ -1,47 +1,78 @@
 use simplex::include_simf;
-use simplex::program::{WitnessTrait, ArgumentsTrait, RandomArguments, RandomWitness};
+use simplex::program::{ArgumentsTrait, RandomArguments, RandomWitness, WitnessTrait};
 
 include_simf!("../../../../crates/simplex/tests/ui_simfs/simple_storage.simf");
 
 fn main() -> Result<(), String> {
+    let _ = test_e2e_behaviour()?;
+    let _ = test_default();
+    let _ = test_e2e_random_behaviour();
+
+    Ok(())
+}
+
+fn test_e2e_behaviour() -> Result<(), String> {
     let original_witness = derived_simple_storage::SimpleStorageWitness::default();
 
     let witness_values = original_witness.build_witness();
-    let recovered_witness = derived_simple_storage::SimpleStorageWitness::from_witness(&witness_values)?;
+    let recovered_witness =
+        derived_simple_storage::SimpleStorageWitness::from_witness(&witness_values)?;
     assert_eq!(original_witness, recovered_witness);
 
     let original_arguments = derived_simple_storage::SimpleStorageArguments::default();
 
     let arguments_values = original_arguments.build_arguments();
-    let recovered_arguments = derived_simple_storage::SimpleStorageArguments::from_arguments(&arguments_values)?;
+    let recovered_arguments =
+        derived_simple_storage::SimpleStorageArguments::from_arguments(&arguments_values)?;
     assert_eq!(original_arguments, recovered_arguments);
 
+    Ok(())
+}
+
+fn test_default() -> Result<(), String> {
+    assert_eq!(
+        derived_simple_storage::SimpleStorageWitness::default(),
+        derived_simple_storage::SimpleStorageWitness::default()
+    );
+    assert_eq!(
+        derived_simple_storage::SimpleStorageArguments::default(),
+        derived_simple_storage::SimpleStorageArguments::default()
+    );
+    Ok(())
+}
+
+fn test_e2e_random_behaviour() -> Result<(), String> {
     for seed in 0..32 {
         use simplex::rand::{rngs::StdRng, SeedableRng};
 
         let mut rng = StdRng::seed_from_u64(seed);
 
-        let original_witness = derived_simple_storage::SimpleStorageWitness::generate_witness_raw(&mut rng);
+        let original_witness =
+            derived_simple_storage::SimpleStorageWitness::generate_witness_raw(&mut rng);
 
         let witness_values = original_witness.build_witness();
-        let recovered_witness = derived_simple_storage::SimpleStorageWitness::from_witness(&witness_values)?;
+        let recovered_witness =
+            derived_simple_storage::SimpleStorageWitness::from_witness(&witness_values)?;
         assert_eq!(original_witness, recovered_witness);
 
         rng = StdRng::seed_from_u64(seed);
-        let rand_raw_witness_values = derived_simple_storage::SimpleStorageWitness::generate_witness(&mut rng);
+        let rand_raw_witness_values =
+            derived_simple_storage::SimpleStorageWitness::generate_witness(&mut rng);
         assert_eq!(witness_values, rand_raw_witness_values);
 
         rng = StdRng::seed_from_u64(seed);
-        let original_arguments = derived_simple_storage::SimpleStorageArguments::generate_arguments_raw(&mut rng);
+        let original_arguments =
+            derived_simple_storage::SimpleStorageArguments::generate_arguments_raw(&mut rng);
 
         let arguments_values = original_arguments.build_arguments();
-        let recovered_arguments = derived_simple_storage::SimpleStorageArguments::from_arguments(&arguments_values)?;
+        let recovered_arguments =
+            derived_simple_storage::SimpleStorageArguments::from_arguments(&arguments_values)?;
         assert_eq!(original_arguments, recovered_arguments);
 
         rng = StdRng::seed_from_u64(seed);
-        let rand_raw_witness_values = derived_simple_storage::SimpleStorageArguments::generate_arguments(&mut rng);
+        let rand_raw_witness_values =
+            derived_simple_storage::SimpleStorageArguments::generate_arguments(&mut rng);
         assert_eq!(arguments_values, rand_raw_witness_values);
     }
-
     Ok(())
 }
