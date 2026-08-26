5195aa42fcaece5123e7b0a9ac4c62bc6d9a5055
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2026-08-26 11:27:18 +0300

    fmt: apply cargo fmt

--- crates/cli/src/cli.rs
+++ crates/cli/src/cli.rs
@@ -83,7 +83,7 @@ impl Cli {
 
                 Ok(Build::run(&loaded_config.build, &loaded_config.dependencies)?)
             }
-            Command::Clean { flags} => {
+            Command::Clean { flags } => {
                 let config_path = Config::get_default_path()?;
                 let loaded_config = Config::load(&config_path)?;
 
