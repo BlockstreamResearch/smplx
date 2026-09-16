bc1e523f3944f8e9d357a89f1f2e8a70eea06a6b
Illia Kripaka <kripakaillya@gmail.com>
2026-09-16 12:00:54 +0300

    fix: typos
    * add default value in UTXO
    * add debug implementation for ProgramInput, FinalInput
    * add Clone for user generated Program by a macro

--- crates/sdk/src/program/arguments.rs
+++ crates/sdk/src/program/arguments.rs
@@ -2,7 +2,7 @@ use dyn_clone::DynClone;
 use simplicityhl::Arguments;
 
 /// An interface for structs capable of generating static argument mapping for Simplicity programs.
-/// See the `include_simc!()` macro, which generates automatic `ArgumentsTrait` implementation.
+/// See the `include_simf!()` macro, which generates automatic `ArgumentsTrait` implementation.
 pub trait ArgumentsTrait: DynClone {
     /// Compiles and returns the bound `Arguments` dict required to instantiate a program.
     fn build_arguments(&self) -> Arguments;
